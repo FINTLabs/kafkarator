14d949b9bf1312c12995909b42e0772637ba8529
vetlewj <vetlewj@gmail.com>
2022-10-18 19:34:35 +0200

    setup base for Controller and Service

---
+++ src/main/java/no/fintlabs/aivenerator/service/AivenService.java
@@ -0,0 +1,10 @@
+package no.fintlabs.aivenerator.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AivenService {
+
+}
