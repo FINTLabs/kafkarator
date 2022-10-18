4b8144723efb3d4e0dced057a696440c8bbd5596
vetlewj <vetlewj@gmail.com>
2022-10-18 15:07:34 +0200

    init project

---
+++ src/main/java/no/fintlabs/aivenerator/AiveneratorApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs.aivenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiveneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiveneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/fintlabs/aivenerator/AiveneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.fintlabs.aivenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiveneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
