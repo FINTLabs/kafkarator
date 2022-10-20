ac52ae0fda9a5fcedfe7d5c002ce2fad2f92c0fa
vetlewj <vetlewj@gmail.com>
2022-10-20 14:46:23 +0200

    create User method in service

--- src/main/java/no/fintlabs/aivenerator/service/AivenService.java
+++ src/main/java/no/fintlabs/aivenerator/service/AivenService.java
@@ -1,10 +1,36 @@
 package no.fintlabs.aivenerator.service;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.aivenerator.model.CreateUserRequest;
+import no.fintlabs.aivenerator.model.CreateUserResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 public class AivenService {
+
+    @Value("${aiven.base_url}")
+    private String baseUrl;
+    @Value("${aiven.token}")
+    private String token;
+
+    public CreateUserResponse createUserForService(String project, String service_name, String username) {
+        String uri = String.format("%s/project/%s/service/%s/user", baseUrl, project, service_name);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername(username);
+
+        HttpEntity<CreateUserRequest> entity = new HttpEntity<>(request, headers);
+        CreateUserResponse response = restTemplate.postForObject(uri, entity, CreateUserResponse.class);
+        return response;
+    }
+    // TODO: Method to create ACL from Topic, Username and permission
 
 }
