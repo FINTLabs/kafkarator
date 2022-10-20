338b1944814bb869782d43c8c8ba5fb66cc5768d
vetlewj <vetlewj@gmail.com>
2022-10-20 14:44:55 +0200

    Update response to map response from Aiven api

--- src/main/java/no/fintlabs/aivenerator/model/CreateUserResponse.java
+++ src/main/java/no/fintlabs/aivenerator/model/CreateUserResponse.java
@@ -6,9 +6,15 @@ import java.io.Serializable;
 
 @Data
 public class CreateUserResponse implements Serializable {
-    private String accessCert;
-    private String accessKey;
-    private String password;
-    private String type;
-    private String username;
+    private String message;
+    private User user;
+
+    @Data
+    public static class User {
+        private String access_cert;
+        private String access_key;
+        private String password;
+        private String type;
+        private String username;
+    }
 }
