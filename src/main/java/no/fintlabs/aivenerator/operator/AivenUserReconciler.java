f11ddca2f46af4d5be1e0deedcb06b8e319cc05a
vetlewj <vetlewj@gmail.com>
2022-10-27 11:04:33 +0200

    implememts EventSourceInitializer, ErrorStatusHandler,  Cleaner

--- src/main/java/no/fintlabs/aivenerator/operator/AivenUserReconciler.java
+++ src/main/java/no/fintlabs/aivenerator/operator/AivenUserReconciler.java
@@ -1,10 +1,11 @@
 package no.fintlabs.aivenerator.operator;
 
 import io.fabric8.kubernetes.api.model.Secret;
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
+import io.javaoperatorsdk.operator.processing.event.source.informer.Mappers;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.aivenerator.model.CreateUserResponse;
 import no.fintlabs.aivenerator.service.AivenService;
@@ -12,10 +13,14 @@ import no.fintlabs.aivenerator.service.SecretService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Slf4j
 @Component
 @ControllerConfiguration
-public class AivenUserReconciler implements Reconciler<AivenUserCrd> {
+public class AivenUserReconciler implements Reconciler<AivenUserCrd>, EventSourceInitializer<AivenUserCrd>,
+        ErrorStatusHandler<AivenUserCrd>,
+        Cleaner<AivenUserCrd> {
 
     private final AivenService aivenService;
     private final SecretService secretService;
@@ -30,10 +35,7 @@ public class AivenUserReconciler implements Reconciler<AivenUserCrd> {
     public UpdateControl<AivenUserCrd> reconcile(AivenUserCrd resource, Context<AivenUserCrd> context) throws Exception {
         log.debug("Reconciling {}", resource.getMetadata().getName());
 
-        // TODO: Fix error
-        // java.lang.IllegalArgumentException: There is no event source found for class:io.fabric8.kubernetes.api.model.Secret
         Secret secret = context.getSecondaryResource(Secret.class).orElse(null);
-
         if (context.getSecondaryResource(Secret.class).isPresent()) {
             log.debug("Secret exists for resource {}", resource.getMetadata().getName());
             return UpdateControl.noUpdate();
@@ -53,5 +55,30 @@ public class AivenUserReconciler implements Reconciler<AivenUserCrd> {
 
     private String uniqId() {
         return RandomStringUtils.randomAlphanumeric(6);
+    }
+
+    @Override
+    public DeleteControl cleanup(AivenUserCrd resource, Context<AivenUserCrd> context) {
+        return null;
+    }
+
+    @Override
+    public ErrorStatusUpdateControl<AivenUserCrd> updateErrorStatus(AivenUserCrd resource, Context<AivenUserCrd> context, Exception e) {
+        AivenUserStatus resourceStatus = resource.getStatus();
+        resourceStatus.setErrorMessage(e.getMessage());
+        resource.setStatus(resourceStatus);
+        return ErrorStatusUpdateControl.updateStatus(resource);
+    }
+
+    @Override
+    public Map<String, EventSource> prepareEventSources(EventSourceContext<AivenUserCrd> context) {
+        return EventSourceInitializer
+                .nameEventSources(
+                        new InformerEventSource<>(
+                                InformerConfiguration.from(Secret.class, context)
+                                        .withSecondaryToPrimaryMapper(Mappers.fromOwnerReference())
+                                        .build(),
+                                context)
+                );
     }
 }
