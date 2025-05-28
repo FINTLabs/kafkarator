bcce365dded7a1a3ed40d2d5c18dea09805af1b3
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2025-05-28 09:06:03 +0200

    set up tests for name factory

--- src/test/groovy/no/fintlabs/operator/NameFactorySpec.groovy
+++ src/test/groovy/no/fintlabs/operator/NameFactorySpec.groovy
@@ -4,10 +4,10 @@ import spock.lang.Specification
 
 class NameFactorySpec extends Specification {
 
-    def "Name should contain orgId and team"() {
+    def "Generated name should include orgId, team and UUID"() {
         given:
         def crd = new KafkaUserAndAclCrd()
-        crd.getMetadata().getLabels().put("fintlabs.no/team", "flais")
+        crd.getMetadata().getLabels().put("fintlabs.no/team", "testteam")
         crd.getMetadata().getLabels().put("fintlabs.no/org-id", "flais.io")
         crd.getMetadata().setName("fint-data-service")
 
@@ -15,6 +15,36 @@ class NameFactorySpec extends Specification {
         def name = NameFactory.nameFromMetadata(crd)
 
         then:
-        name == "flais-io_flais_fint-data-service"
+        def parts = name.split("-")
+        parts.length >= 4
+
+        and: "UUID Should me last and 16 characters"
+        parts[-1] ==~ /^[a-f0-9]{16}$/
+
+        and: "Final string should be max 64 characters"
+        name.length() <= 64
+
+        and: "OrgID, team, and name should be included"
+        name.contains("test")
+        name.contains("flais")
+        name.contains("fint")
+    }
+
+    def "Should handle long orgId, team and name correctly"() {
+        given:
+        def crd = new KafkaUserAndAclCrd()
+        crd.getMetadata().getLabels().put("fintlabs.no/team", "very-long-team-name-that-blows-all-borders")
+        crd.getMetadata().getLabels().put("fintlabs.no/org-id", "insanely.long.organisation.identifier.that.exceeds.all.limits")
+        crd.getMetadata().setName("well-ok-we-know-this-name-is-too-long-now-right")
+
+        when:
+        def name = NameFactory.nameFromMetadata(crd)
+
+        then:
+        name.length() <= 64
+
+        and: def uuid = name.split("-")[-1]
+        uuid.length() == 16
+        uuid ==~ /^[a-f0-9]{16}$/
     }
 }
