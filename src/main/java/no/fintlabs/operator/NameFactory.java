bd334759000d156a6b76f218b279b50d7fc33fd3
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2025-06-02 15:08:12 +0200

    rewrite to just GUID, and support legacy code

--- src/main/java/no/fintlabs/operator/NameFactory.java
+++ src/main/java/no/fintlabs/operator/NameFactory.java
@@ -6,29 +6,24 @@ import java.util.UUID;
 
 public class NameFactory {
 
+    public static String legacyNameFromMetadata(HasMetadata metadata) {
+        return String.format("%s_%s_%s",
+                metadata.getMetadata().getLabels().get("fintlabs.no/org-id").replace(".", "-"),
+                metadata.getMetadata().getLabels().get("fintlabs.no/team"),
+                metadata.getMetadata().getName()
+        );
+    }
+
+    public static String guidNameFromMetadata() {
+        return UUID.randomUUID().toString();
+    }
+
     public static String nameFromMetadata(HasMetadata metadata) {
-
-        String orgId = metadata.getMetadata().getLabels().get("fintlabs.no/org-id").replace(".", "-");
-        String team = metadata.getMetadata().getLabels().get("fintlabs.no/team");
-        String name = metadata.getMetadata().getName();
-        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 16);
-
-        int maxCombinedPrefix = 64 - 19;
-        int totalPrefixLength = orgId.length() + team.length() + name.length();
-        if (totalPrefixLength > maxCombinedPrefix) {
-            double orgRatio = (double) orgId.length() / totalPrefixLength;
-            double teamRatio = (double) team.length() / totalPrefixLength;
-            double nameRatio = (double) name.length() / totalPrefixLength;
-
-            int maxOrgLength = (int) Math.floor(maxCombinedPrefix * orgRatio);
-            int maxTeamLength = (int) Math.floor(maxCombinedPrefix * teamRatio);
-            int maxNameLength = maxCombinedPrefix - maxOrgLength - maxTeamLength;
-
-            orgId = orgId.substring(0, Math.min(orgId.length(), maxOrgLength));
-            team = team.substring(0, Math.min(team.length(), maxTeamLength));
-            name = name.substring(0, Math.min(name.length(), maxNameLength));
+        String useGuid = metadata.getMetadata().getLabels().get("fintlabs.no/use-guid");
+        if ("true".equalsIgnoreCase(useGuid)) {
+            return guidNameFromMetadata();
+        } else {
+            return legacyNameFromMetadata(metadata);
         }
-
-        return String.format("%s-%s-%s-%s", orgId, team, name, uuid);
     }
 }
