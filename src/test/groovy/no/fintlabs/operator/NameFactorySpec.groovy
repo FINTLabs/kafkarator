a191133de554fb11b37bff0c702054acee075422
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2025-06-05 12:26:00 +0200

    added tests

--- src/test/groovy/no/fintlabs/operator/NameFactorySpec.groovy
+++ src/test/groovy/no/fintlabs/operator/NameFactorySpec.groovy
@@ -4,47 +4,53 @@ import spock.lang.Specification
 
 class NameFactorySpec extends Specification {
 
-    def "Generated name should include orgId, team and UUID"() {
+    def "Legacy should include orgId, team and name"() {
         given:
         def crd = new KafkaUserAndAclCrd()
-        crd.getMetadata().getLabels().put("fintlabs.no/team", "testteam")
+        crd.getMetadata().getLabels().put("fintlabs.no/team", "test-team")
         crd.getMetadata().getLabels().put("fintlabs.no/org-id", "flais.io")
+        crd.getMetadata().setName("fint-data-service")
+
+        when:
+        def name = NameFactory.legacyNameFromMetadata(crd)
+
+        then:
+        name.equals("flais-io_test-team_fint-data-service")
+    }
+
+    def "guidNameFromMetadata should return valid UUID format"() {
+        when:
+        def guid = NameFactory.guidNameFromMetadata()
+
+        then:
+        guid.matches(/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/)
+        guid.length() <= 64
+    }
+
+    def "nameFromMetadata should return GUID name is use-guid is set to true"() {
+        given:
+        def crd = new KafkaUserAndAclCrd()
+        crd.getMetadata().getLabels().put("fintlabs.no/use-guid", "true")
+
+        when:
+        def name = NameFactory.nameFromMetadata(crd)
+
+        then:
+        name.matches(/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/)
+    }
+
+    def "nameFromMetadata should return legacy name if use-guid is not true"() {
+        given:
+        def crd = new KafkaUserAndAclCrd()
+        crd.getMetadata().getLabels().put("fintlabs.no/team", "test-team")
+        crd.getMetadata().getLabels().put("fintlabs.no/org-id", "flais.io")
+        crd.getMetadata().getLabels().put("fintlabs.no/use-guid", "false")
         crd.getMetadata().setName("fint-data-service")
 
         when:
         def name = NameFactory.nameFromMetadata(crd)
 
         then:
-        def parts = name.split("-")
-        parts.length >= 4
-
-        and: "UUID Should me last and 16 characters"
-        parts[-1] ==~ /^[a-f0-9]{16}$/
-
-        and: "Final string should be max 64 characters"
-        name.length() <= 64
-
-        and: "OrgID, team, and name should be included"
-        name.contains("test")
-        name.contains("flais")
-        name.contains("fint")
-    }
-
-    def "Should handle long orgId, team and name correctly"() {
-        given:
-        def crd = new KafkaUserAndAclCrd()
-        crd.getMetadata().getLabels().put("fintlabs.no/team", "very-long-team-name-that-blows-all-borders")
-        crd.getMetadata().getLabels().put("fintlabs.no/org-id", "insanely.long.organisation.identifier.that.exceeds.all.limits")
-        crd.getMetadata().setName("well-ok-we-know-this-name-is-too-long-now-right")
-
-        when:
-        def name = NameFactory.nameFromMetadata(crd)
-
-        then:
-        name.length() <= 64
-
-        and: def uuid = name.split("-")[-1]
-        uuid.length() == 16
-        uuid ==~ /^[a-f0-9]{16}$/
+        name.equals("flais-io_test-team_fint-data-service")
     }
 }
