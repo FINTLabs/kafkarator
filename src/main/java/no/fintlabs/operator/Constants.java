2ff1d7162bb7636fde5b2f8c539a72670ae250bf
Sondre Nordjore <sondre.nordjore@vigoiks.no>
2026-06-18 11:56:47 +0200

    Moved constants to seperate class

---
+++ src/main/java/no/fintlabs/operator/Constants.java
@@ -0,0 +1,12 @@
+package no.fintlabs.operator;
+
+public class Constants {
+    static final String ORG_ID_LABEL = "fintlabs.no/org-id";
+    static final String TEAM_LABEL = "fintlabs.no/team";
+    static final int ORG_NAME_LENGTH = 10;
+    static final int TEAM_NAME_LENGTH = 15;
+    static final int APP_NAME_LENGTH = 25;
+    static final String SHOULD_USE_NAME_V2 = "SHOULD_USE_NAME_V2";
+    static final String NAME_VERSION_ANNOTATION = "kafka.fintlabs.no/name-version";
+    static final String NAME_VERSION_V2 = "v2";
+}
